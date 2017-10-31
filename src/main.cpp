5c2b53b22e79a3d574031c6470f716f03f356fbf
geekymoose <constantin.masson@umontreal.ca>
2017-10-31 14:01:16 -0400

    Add CMake build system

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,11 @@
 
 // Just for test
 int main(int argc, char** argv) {
-    LOG_DEBUG("TEST DEBUG");
+    LOG_ERROR("TEST ERROR");
+    LOG_WARNING("TEST WARNING");
     LOG_CONFIG("TEST CONFIG");
+    LOG_INFO("TEST INFO");
+    LOG_TRACE("TEST TRACE");
+    LOG_DEBUG("TEST DEBUG");
     return 0;
 }
