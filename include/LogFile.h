35436d7a1fa80b6199f86f5d1e999d8c22cc4867
geekymoose <constantin.masson@umontreal.ca>
2017-11-15 20:05:33 -0500

    Create LogFile class

---
+++ include/LogFile.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include <string>
+#include <mutex>
+#include <fstream>
+
+
+namespace ElephantLogger {
+
+class LogMessage;
+
+
+/**
+ * File logging output.
+ *
+ * \author  Constantin Masson
+ * \date    Nov 2017
+ */
+class LogFile {
+    private:
+        /** Path of the log file (Without it's name). */
+        std::string m_filePath;
+
+        /** Name of the log file. */
+        std::string m_fileName;
+
+        /** Combination of path + file name. */
+        std::string m_fullPath;
+
+        /** Stream to the file. May be opened during all LogFile lifetime. */
+        std::ofstream m_stream;
+
+        /** Mutex for thread safe methods. */
+        mutable std::mutex m_streamAccess;
+
+
+    public:
+
+        /**
+         * Create a new log file.
+         * If filePath doesn't exists, tries to create id. (Parent must exists).
+         * If file doesn't exists, create it, otherwise, open it (Append mode).
+         *
+         * \warning
+         * If it's impossible to create the directory,
+         * all methods return false and do nothing.
+         *
+         * \param filePath Directory where file may live.
+         * \param fileName Name of the file.
+         */
+        LogFile(std::string filePath, std::string fileName);
+
+        /**
+         * Close file stream and destroye this poor object.
+         */
+        ~LogFile();
+
+
+    public:
+
+        /**
+         * Write a log message in this file and add a line return.
+         *
+         * \note
+         * This function is Thread safe.
+         *
+         * \param message The message to write.
+         */
+        void write(const LogMessage & message);
+
+        /**
+         * Save the content of the log file.
+         * Copy is placed in the same directory, but with current timestamp suffix.
+         *
+         * \note
+         * This function is Thread safe.
+         *
+         * \return True if saved successfully, otherwise, return false.
+         */
+        bool save() const;
+
+        /**
+         * Clear content of the log file.
+         * This function is Thread safe.
+         */
+        void clear();
+
+    private:
+        bool internal_save(std::string & savePath) const;
+};
+
+
+} // End namespace
---
+++ src/LogFile.cpp
@@ -0,0 +1,82 @@
+#include "LogFile.h"
+#include "LogMessage.h"
+
+#include <experimental/filesystem>
+#include <chrono>
+
+namespace fs = std::experimental::filesystem;
+
+using namespace ElephantLogger;
+
+
+// -----------------------------------------------------------------------------
+// Constructors
+// -----------------------------------------------------------------------------
+
+LogFile::LogFile(std::string filePath, std::string fileName)
+  : m_filePath(filePath), m_fileName(fileName) {
+
+    fs::create_directory(this->m_filePath);
+    this->m_fullPath = this->m_filePath + this->m_fileName;
+    this->m_stream.open(this->m_fullPath);
+}
+
+LogFile::~LogFile() {
+    if(this->m_stream.is_open()) {
+        this->m_stream.close();
+    }
+}
+
+
+// -----------------------------------------------------------------------------
+// Core public methods
+// -----------------------------------------------------------------------------
+
+void LogFile::write(const LogMessage & message) {
+    std::lock_guard<std::mutex> lock(m_streamAccess);
+
+    if(this->m_stream.is_open()) {
+        //TODO
+    }
+}
+
+bool LogFile::save() const {
+    using Clock = std::chrono::system_clock;
+    std::time_t startTime = Clock::to_time_t(Clock::now());
+
+    char timestamp[20];
+    std::strftime(timestamp, 20, "%Y_%m_%d_%H%M%S", std::localtime(&startTime));
+
+    std::lock_guard<std::mutex> lock(m_streamAccess);
+
+    std::string destination = this->m_filePath + "/" + timestamp + "_" + this->m_fileName;
+    return this->internal_save(destination);
+}
+
+void LogFile::clear() {
+    std::lock_guard<std::mutex> lock(m_streamAccess);
+
+    if(this->m_stream.is_open()) {
+        this->m_stream.close();
+        this->m_stream.open(this->m_fullPath, std::ofstream::trunc);
+    }
+}
+
+
+// -----------------------------------------------------------------------------
+// Internal
+// -----------------------------------------------------------------------------
+
+bool LogFile::internal_save(std::string& savePath) const {
+    if(this->m_stream.is_open()) {
+        auto destination = fs::path(savePath);
+
+        if(fs::exists(destination) && !fs::is_directory(destination)) {
+            return false; // Doesn't override existing file.
+        }
+
+        fs::copy(this->m_fullPath, destination);
+        return true;
+    }
+    return false;
+}
